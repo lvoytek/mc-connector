8b913f96d3e67529b5143d859010d02e249ce53b
Lena Voytek <lena.voytek@canonical.com>
2024-11-19 18:16:28 -0700

    Split out jar file and path

--- src/mcconnector.ts
+++ src/mcconnector.ts
@@ -1,4 +1,5 @@
 import { Client } from "discord.js";
+import * as path from "path";
 import ready from "./listeners/ready";
 import { config } from "./config";
 import {spawn, ChildProcess} from "child_process";
@@ -11,9 +12,10 @@ var mcDaemon: ChildProcess;
 
 function startMinecraft(): void {
     const command = 'java'
-    const args = ['-Xmx8192M', '-Xms128M', '-jar', config.ServerJarFile, 'nogui']
+    const args = ['-Xmx8192M', '-Xms128M', '-jar', path.basename(config.ServerJarFile), 'nogui']
 
     mcDaemon = spawn(command, args, {
+        cwd: path.dirname(config.ServerJarFile),
         detached: true,
         stdio: ['ignore', 'pipe', 'pipe']
     });
