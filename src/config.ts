640bba7defffe4ff5642887bbed7ebac904e96bb
Lena Voytek <lena.voytek@canonical.com>
2024-11-19 17:10:43 -0700

    Add server jar file location config option

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    Token: process.env.TOKEN ?? ''
+    Token: process.env.TOKEN ?? '',
+    ServerJarFile: process.env.SERVER_JAR_FILE ?? 'server.jar'
 }
