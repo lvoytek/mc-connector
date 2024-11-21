2d426393e70925908ca4930bb9f805e98a61d9bd
Lena Voytek <lena.voytek@canonical.com>
2024-11-21 16:31:25 -0700

    Add command type and command deployment

---
+++ src/command.ts
@@ -0,0 +1,6 @@
+import { CommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from "discord.js";
+
+export interface Command {
+    data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
+    execute: (interaction: CommandInteraction) => void;
+}
---
+++ src/deploy-commands.ts
@@ -0,0 +1,27 @@
+import { REST, Routes } from "discord.js";
+import { config } from "./config";
+import { Commands } from "./commands";
+
+const commandJSONSet = []
+
+for (const command of Commands) {
+    commandJSONSet.push(command.data.toJSON())
+}
+
+const rest: REST = new REST().setToken(config.Token);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commandJSONSet.length} application (/) commands.`);
+
+		await rest.put(
+			Routes.applicationCommands(config.ClientID),
+			{ body: commandJSONSet },
+		);
+
+		console.log(`Successfully reloaded application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
