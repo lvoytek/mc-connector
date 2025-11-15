628aecd5da31f27cf6abea5b48911a5ad3071ec1
Lena Voytek <lena@voytek.dev>
2025-11-15 13:04:25 -0500

    Add LogHost option in config

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,6 @@ dotenv.config();
 export const config = {
   Token: process.env.TOKEN ?? "",
   ClientID: process.env.CLIENT_ID ?? "",
+  LogHost: process.env.MINECRAFT_LOG_HOST ?? "127.0.0.1",
   LogPort: parseInt(process.env.MINECRAFT_LOG_PORT ?? "25566", 10),
 };
